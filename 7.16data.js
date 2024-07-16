c6db3c3d5bdf8d429460236071a91c2af93e2f61
erkaerka21 <eegiibaagii597@gmail.com>
2024-07-17 07:37:58 +0800

    7.16 borluulalt

---
+++ 7.16data.js
@@ -0,0 +1,100 @@
+const data = [
+  {
+    productName: "Bakery",
+    unitPrice: 5000,
+    amount: 200,
+    totalPrice: 500000,
+    casherId: 1,
+    date: "2022-11-01",
+  },
+  {
+    productName: "Milk",
+    unitPrice: 3000,
+    amount: 18,
+    totalPrice: 5400,
+    casherId: 1,
+    date: "2022-11-02",
+  },
+  {
+    productName: "Cookie",
+    unitPrice: 2800,
+    amount: 198,
+    totalPrice: 670000,
+    casherId: 1,
+    date: "2022-11-01",
+  },
+  {
+    productName: "Chocolate",
+    unitPrice: 3000,
+    amount: 180,
+    totalPrice: 1909000,
+    casherId: 1,
+    date: "2022-11-01",
+  },
+  {
+    productName: "Chocolate-candy",
+    unitPrice: 5000,
+    amount: 67,
+    totalPrice: 900000,
+    casherId: 1,
+    date: "2022-11-02",
+  },
+  {
+    productName: "Coca-cola",
+    unitPrice: 30,
+    amount: 18,
+    totalPrice: 28000,
+    casherId: 1,
+    date: "2022-11-01",
+  },
+  {
+    productName: "Orange-Juice",
+    unitPrice: 500000,
+    amount: 200,
+    totalPrice: 4890000,
+    casherId: 1,
+    date: "2022-11-02",
+  },
+  {
+    productName: "Banana",
+    unitPrice: 4890,
+    amount: 9000,
+    totalPrice: 3243200,
+    casherId: 1,
+    date: "2022-11-02",
+  },
+  {
+    productName: "Protein-bar",
+    unitPrice: 17000,
+    amount: 1000,
+    totalPrice: 17000000,
+    casherId: 1,
+    date: "2022-11-01",
+  },
+  {
+    productName: "Protein-shake",
+    unitPrice: 30070,
+    amount: 456,
+    totalPrice: 569000,
+    casherId: 1,
+    date: "2022-11-02",
+  },
+];
+console.log("Борлуулалтын мэдээ : ", data);
+
+let findOrangeJuice = data.findIndex((i) => i.productName === "Orange-Juice");
+console.log("Жүржийн шүүсний байрших дугаар : ", findOrangeJuice);
+let orangeJuiceTotalPrice =
+  data[data.findIndex((i) => i.productName === "Orange-Juice")].totalPrice;
+console.log(
+  "Жүржийн шүүсний борлуулалтын нийт үнийн дүн : ",
+  orangeJuiceTotalPrice
+);
+
+const bodsonTotalPrice = data.map((i, unitPrice, amount) => unitPrice * amount);
+console.log(bodsonTotalPrice);
+
+const maxTotalPriceIn1102 = data.filter(
+  (i) => data[i].date === "2022 - 11 - 02"
+);
+console.log(maxTotalPriceIn1102);
